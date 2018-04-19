cc5d43c7d13c89c8f4d1db9922b693e6189f831c
Pravin Bendre <pravinbendre772@gmail.com>
2018-04-19 19:26:55 +0530

    Updated jinja2

---
+++ app/toys.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/codes/')
+def codes():
+    return render_template('codes.html')
+
+@app.route('/portfolio/')
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/signup/')
+def signup():
+    return render_template('signup.html')
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
